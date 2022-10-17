0fcba9971375a2f9babae5ac118d0d4d73fad2cf
Cratch <Cratcism@mail.ru>
2022-10-18 02:39:38 +0300

    add main.c file

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a, min=21, max=56
+    scanf ("%d", &a);
+    printf ("%d\n", min<a && max>a);
+    int b;
+    scanf ("%d", &b);
+    printf ("%d\n",(b >> 11)%2 );
+
+    return 0;
+
+
+}
